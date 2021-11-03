c6d57e6e46dd001ba683880e29cb227db4c4cb81
Kiran Manicka <kmanicka013@gmail.com>
2021-11-03 18:55:09 -0400

    template

---
+++ addTransaction.php
@@ -0,0 +1,34 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h1>Add a new Transaction</h1>
+
+
+<form>
+  <label for="fname">Name:</label><br>
+  <input type="text" id="fname" name="fname"><br>
+  <label for="lname">Date:</label><br>
+  <input type="text" id="lname" name="lname">
+  <label for="fname">Amount:</label><br>
+  <input type="text" id="fname" name="fname"><br>
+  <label for="lname">Type:</label><br>
+  <input type="radio" id="html" name="fav_language" value="HTML">
+  <label for="html">Debit</label><br>
+  <input type="radio" id="css" name="fav_language" value="CSS">
+  <label for="css">Card</label><br>
+  <input type="submit" value="Submit">
+</form>
+
+<button>Add Transaction</button>
+
+
+</body>
+</html>
